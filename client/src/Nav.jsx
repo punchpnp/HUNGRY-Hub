3ca60a5a2f8d644ea238937c42dd71fabd7a2da4
DESKTOP-VVN7SIU\User <nunnapatppp@gmail.com>
2023-12-05 13:29:30 +0700

    done! install tailwind

---
+++ client/src/Nav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <>
+      <div className="text-7xl">Navvvv</div>
+    </>
+  );
+}
+
+export default Nav;
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// //vitejs.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// // })
+
+// https: export default defineConfig({
+//   plugins: [react()],
+//   css: {
+//     postcss: {
+//       plugins: [require("tailwindcss"), require("autoprefixer")],
+//     },
+//   },
+// });
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import VitePluginWindiCSS from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePluginWindiCSS()],
+});
