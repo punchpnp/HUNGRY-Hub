bfadd3dfdea6afbd2f4c8230042331cbefaba095
DESKTOP-VVN7SIU\User <nunnapatppp@gmail.com>
2023-12-07 01:09:58 +0700

    🟡 wait check (home)

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -12,14 +12,13 @@ import { BsEye } from "react-icons/bs";
 import { AiOutlineHeart } from "react-icons/ai";
 import { IoIosCloseCircle } from "react-icons/io";
 
-import KhaiJiao from "/src/assets/Khai-jiao.jpg";
+import Hungry from "/src/assets/hungry.png";
 import Mama from "/src/assets/mama.jpg";
 
 import "./Home.css";
 import Homeproduct from "./Homeproduct";
 
 function Home({ detail, view, close, setClose, addtocart }) {
-  // const [homeproduct, sethomeproduct] = usestate(Homeproduct);
   return (
     <>
       {close ? (
@@ -52,117 +51,19 @@ function Home({ detail, view, close, setClose, addtocart }) {
           </div>
         </div>
       ) : null}
-      {/* top_banner สำหรับโฆษณา */}
+
+      {/* top_banner*/}
       <div className="top_banner">
         <div className="container">
           <div className="detail">
-            <h2>The Best Note Book Collection 2023</h2>
+            <h2>Are you hungry today?</h2>
+
             <Link to="product" className="link">
-              Shop Now <BsArrowRight />
+              Eat Now <BsArrowRight />
             </Link>
           </div>
           <div className="img_box">
-            <img src={KhaiJiao} alt="" />
-          </div>
-        </div>
-      </div>
-
-      {/* สินค้าต่างๆ */}
-      <div className="product_type">
-        <div className="container">
-          <div className="box">
-            <div className="img_box">
-              <img src={Mama} alt="mama-pud" />
-            </div>
-
-            <div className="detail">
-              <p>23 products</p>
-            </div>
-          </div>
-
-          <div className="box">
-            <div className="img_box">
-              <img src={Mama} alt="mama-pud" />
-            </div>
-
-            <div className="detail">
-              <p>23 products</p>
-            </div>
-          </div>
-
-          <div className="box">
-            <div className="img_box">
-              <img src={Mama} alt="mama-pud" />
-            </div>
-
-            <div className="detail">
-              <p>23 products</p>
-            </div>
-          </div>
-
-          <div className="box">
-            <div className="img_box">
-              <img src={Mama} alt="mama-pud" />
-            </div>
-
-            <div className="detail">
-              <p>23 products</p>
-            </div>
-          </div>
-
-          <div className="box">
-            <div className="img_box">
-              <img src={Mama} alt="mama-pud" />
-            </div>
-
-            <div className="detail">
-              <p>23 products</p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* สิทธิพิเศษ */}
-      <div className="about">
-        <div className="container">
-          <div className="box">
-            <div className="icon">
-              <FaBahtSign />
-            </div>
-            <div className="detail">
-              <h3>Free Shipping</h3>
-              <p>Order above ฿50</p>
-            </div>
-          </div>
-
-          <div className="box">
-            <div className="icon">
-              <HiOutlineReceiptPercent />
-            </div>
-            <div className="detail">
-              <h3>Return & Refund</h3>
-              <p>Money Back Gaurenty</p>
-            </div>
-          </div>
-
-          <div className="box">
-            <div className="icon">
-              <BiHeadphone />
-            </div>
-            <div className="detail">
-              <h3>Member Discout</h3>
-              <p>On every Order</p>
-            </div>
-          </div>
-
-          <div className="box">
-            <div className="icon">
-              <CiPercent />
-            </div>
-            <div className="detail">
-              <h3>Customer Support</h3>
-              <p>Every Time call Support</p>
-            </div>
+            <img src={Hungry} alt="hungry" />
           </div>
         </div>
       </div>
@@ -204,16 +105,16 @@ function Home({ detail, view, close, setClose, addtocart }) {
       <div className="banner">
         <div className="container">
           <div className="detail">
-            <h4>LASTEST TECHNOLOGY ADDED</h4>
-            <h3>Apple iPad 10.2 9th Gen-2021</h3>
-            <p>฿ 10000</p>
+            <h4>LOL we hungry very much</h4>
+            <h3>Do you need to eat all my shop?</h3>
+            <p>Pay 1,000,000 ฿</p>
             <Link to="/product" className="link">
-              Shop Now <BsArrowRight />
+              Eat Now <BsArrowRight />
             </Link>
           </div>
 
           <div className="img_box">
-            <img src={KhaiJiao} alt="khaijiao" />
+            <img src={Hungry} alt="Hungry" />
           </div>
         </div>
       </div>
