209ce176ba5de6742aa040acb5a11d0ba8f2cc33
DESKTOP-VVN7SIU\User <nunnapatppp@gmail.com>
2023-12-06 18:35:01 +0700

    contact update

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Contact.css";
+import { useAuth0 } from "@auth0/auth0-react";
 function Contact() {
+  //
+  const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
+
+  const [users, setUser] = useState({
+    Name: "",
+    Email: "",
+    Subject: "",
+    Message: "",
+  });
+
+  //
+  let name, value;
+  const data = (e) => {
+    name = e.target.name;
+    value = e.target.value;
+    setUser({ ...users, [name]: value });
+  };
+
+  //
+  const senddata = async (e) => {
+    const { Name, Email, Subject, Message } = users;
+    e.preventDefault();
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        Name,
+        Email,
+        Subject,
+        Message,
+      }),
+    };
+    try {
+      const res = await fetch(
+        "https://e-commerce-contact-d0a4d-default-rtdb.firebaseio.com/Message.json",
+        options
+      );
+
+      if (res.ok) {
+        alert("Your Message sent");
+      } else {
+        alert("An Error occurred");
+      }
+    } catch (error) {
+      console.error("Error sending data:", error);
+      alert("An unexpected error occurred");
+    }
+
+    return false;
+  };
   return (
     <>
       <div className="contact_container">
@@ -12,34 +65,46 @@ function Contact() {
               <input
                 type="text"
                 name="Name"
-                value=""
+                value={users.Name}
                 placeholder="Enter Your Full Name"
                 required
                 autoComplete="off"
+                onChange={data}
               />
               <input
                 type="email"
-                name="E-mail"
-                value=""
+                name="Email"
+                value={users.Email}
                 placeholder="Enter Your E-mail"
                 required
                 autoComplete="off"
+                onChange={data}
               />
               <input
                 type="subject"
                 name="Subject"
-                value=""
+                value={users.Subject}
                 placeholder="Enter Your Subject"
                 required
                 autoComplete="off"
+                onChange={data}
               />
               <textarea
                 name="Message"
-                value=""
+                value={users.Message}
                 placeholder="Your Message"
                 autoComplete="off"
+                onChange={data}
               ></textarea>
-              <button type="submit">Send</button>
+              {isAuthenticated ? (
+                <button type="submit" onClick={senddata}>
+                  Send
+                </button>
+              ) : (
+                <button type="submit" onClick={() => loginWithRedirect()}>
+                  Login
+                </button>
+              )}
             </form>
           </div>
         </div>
