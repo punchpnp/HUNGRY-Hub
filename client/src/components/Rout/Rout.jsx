c1c8e980e51dbe38d6bca20e387f1ab023cddaa5
DESKTOP-VVN7SIU\User <nunnapatppp@gmail.com>
2023-12-06 09:43:46 +0700

    craete Product page

--- client/src/components/Rout/Rout.jsx
+++ client/src/components/Rout/Rout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../Home/Home";
+import Product from "../Product/Product";
 
 function Rout() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
       </Routes>
     </>
   );
